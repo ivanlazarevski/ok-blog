f03e858931abb75df75e6370d17e2adfe8cc23f6
Ivan Lazarevski <ivan.lazarevski@avenga.com>
2025-11-28 16:53:50 +0100

    Last try

--- mynode.js
+++ mynode.js
@@ -2,18 +2,22 @@ const fs = require('fs');
 const path = require('path');
 const successColor = '\x1b[32m%s\x1b[0m';
 const checkSign = '\u{2705}';
-const dotenv = require('dotenv').config({path: 'src/.env'}); ;
+const dotenv = require('dotenv').config({ path: 'src/.env' });
 
+// Instead of using process.env directly in the Angular file, use import.meta.env
 const envFile = `export const environment = {
-    siteKey: '${process.env.siteKey}',
+    siteKey: import.meta.env.siteKey,
+    production: false
 };
 `;
+
 const targetPath = path.join(__dirname, './src/environments/environment.development.ts');
+
 fs.writeFile(targetPath, envFile, (err) => {
-    if (err) {
-        console.error(err);
-        throw err;
-    } else {
-        console.log(successColor, `${checkSign} Successfully generated environment.development.ts`);
-    }
+  if (err) {
+    console.error(err);
+    throw err;
+  } else {
+    console.log(successColor, `${checkSign} Successfully generated environment.development.ts`);
+  }
 });
