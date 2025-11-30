278a703bb208d616ac6ca439daeffce02dd4f1a3
ivanlazarevski <dev.ivanlazarevski@outlook.com>
2025-11-30 13:10:19 +0100

    Added vercel analytics

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { FooterComponent } from './components/footer/footer.component';
+import { inject } from '@vercel/analytics';
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Header, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {}
+export class App {
+  constructor() {
+    inject();
+  }
+}
