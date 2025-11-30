20f1f6623d15b7e17abd7d3c9bb785fa90b34bd0
ivanlazarevski <dev.ivanlazarevski@outlook.com>
2025-11-30 11:22:23 +0100

    Removed scroll to top

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,29 +1,11 @@
-import { Component, HostListener, inject, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { FooterComponent } from './components/footer/footer.component';
-import { ViewportScroller } from '@angular/common';
-
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Header, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {
-  private readonly viewportScroller = inject(ViewportScroller);
-  public scrollPosition = signal(0);
-
-  @HostListener("window:scroll")
-  onWindowScroll() {
-    const offset =
-      window.pageYOffset ||
-      document.documentElement.scrollTop ||
-      document.body.scrollTop || 0;
-    this.scrollPosition.set(offset);
-  }
-
-  public goToTop(): void {
-    document.documentElement.scrollTop = 0;
-  }
-}
+export class App {}
