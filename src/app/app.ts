1cae3d69d284f8f89a0b4e1831258048314cb422
ivanlazarevski <dev.ivanlazarevski@outlook.com>
2025-11-30 11:19:02 +0100

    Added scroll to top and a footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,29 @@
-import { Component, signal } from '@angular/core';
+import { Component, HostListener, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {Header} from './components/header/header';
+import { Header } from './components/header/header';
+import { FooterComponent } from './components/footer/footer.component';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, FooterComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('ok-blog');
+  private readonly viewportScroller = inject(ViewportScroller);
+  public scrollPosition = signal(0);
+
+  @HostListener("window:scroll")
+  onWindowScroll() {
+    const offset =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop || 0;
+    this.scrollPosition.set(offset);
+  }
+
+  public goToTop(): void {
+    document.documentElement.scrollTop = 0;
+  }
 }
